a4eea42f6c5bff8052c05d5b061a48ab12d0cf26
Nazzy-1 <nnazirite@gmail.com>
2023-08-25 19:49:47 -0700

    task 1

---
+++ int_parse.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int convert_int (int num, char *str)
+{
+	int i = 0;
+	int check_neg = 0;
+
+	if (num < 0)
+	{
+		check_neg = 1;
+		num = -num;
+	}
+
+	while (num)
+	{
+		int digit = num % 10;
+		str[i++] = digit + '0';
+		num = num / 10;
+	}
+	
+	if (check_neg)
+		str[i++] = '-';
+	str[i] = '\0';
+	rev_str(str, i);
+
+	return (i);
+}
+
+void rev_str( char *str, int len)
+{
+	int init;
+	int fin;
+
+	init = 0;
+	fin = len - 1;
+
+	while (init < fin)
+	{
+		char tmp = str[init];
+		str[init] = str[fin];
+		str[fin] = tmp;
+		init++;
+		fin--;
+	}
+}
